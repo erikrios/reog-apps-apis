63bbc845898e34fcfba209b822b44d101625e91d
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2022-04-16 10:42:11 +0700

    Create the implementation of admin repository interface

---
+++ repository/admin/admin_repository_impl.go
@@ -0,0 +1,30 @@
+package admin
+
+import (
+	"context"
+	"errors"
+
+	"github.com/erikrios/reog-apps-apis/entity"
+	"github.com/erikrios/reog-apps-apis/repository"
+	"gorm.io/gorm"
+)
+
+type adminRepositoryImpl struct {
+	db *gorm.DB
+}
+
+func NewAdminRepositoryImpl(db *gorm.DB) *adminRepositoryImpl {
+	return &adminRepositoryImpl{db: db}
+}
+
+func (a *adminRepositoryImpl) FindByUsername(ctx context.Context, username string) (admin entity.Admin, err error) {
+	if dbErr := a.db.First(&admin, "username = ?", username).Error; dbErr != nil {
+		if errors.Is(dbErr, gorm.ErrRecordNotFound) {
+			err = repository.ErrRecordNotFound
+			return
+		}
+		err = repository.ErrDatabase
+		return
+	}
+	return
+}
