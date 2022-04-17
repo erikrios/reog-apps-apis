c6c1b78681f1921625b451062c5cefcf45820449
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2022-04-17 10:28:30 +0700

    Refactor the code

--- repository/address/address_repository_impl.go
+++ repository/address/address_repository_impl.go
@@ -17,8 +17,22 @@ func NewAddressRepositoryImpl(db *gorm.DB) *addressRepositoryImpl {
 }
 
 func (a *addressRepositoryImpl) Update(ctx context.Context, address entity.Address) (err error) {
-	if dbErr := a.db.WithContext(ctx).Save(&address).Error; dbErr != nil {
+	if result := a.db.WithContext(ctx).Model(&address).Updates(entity.Address{
+		Address:      address.Address,
+		VillageID:    address.VillageID,
+		VillageName:  address.VillageName,
+		DistrictID:   address.DistrictID,
+		DistrictName: address.DistrictName,
+		RegencyID:    address.RegencyID,
+		RegencyName:  address.RegencyName,
+		ProvinceID:   address.ProvinceID,
+		ProvinceName: address.ProvinceName,
+	}); result.Error != nil {
 		err = repository.ErrRecordNotFound
+	} else {
+		if result.RowsAffected < 1 {
+			err = repository.ErrRecordNotFound
+		}
 	}
 	return
 }
