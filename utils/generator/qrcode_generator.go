72ce47587d9f0066be07080fa0addf52fe0eb1bb
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2022-04-18 11:12:12 +0700

    Create qrcode_generator.go

--- utils/generator/qrcode_generator.go
+++ utils/generator/qrcode_generator.go
@@ -2,6 +2,6 @@ package generator
 
 import qrcode "github.com/skip2/go-qrcode"
 
-func Generate(content string, level qrcode.RecoveryLevel, size int) ([]byte, error) {
+func GenerateQRCode(content string, level qrcode.RecoveryLevel, size int) ([]byte, error) {
 	return qrcode.Encode(content, level, size)
 }
