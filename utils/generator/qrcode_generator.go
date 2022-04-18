504bf9e6b3fb05ca8f0601e03ca11ab32651bdb4
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2022-04-18 18:50:21 +0700

    Refactor the code

--- utils/generator/qrcode_generator.go
+++ utils/generator/qrcode_generator.go
@@ -1,6 +1,8 @@
 package generator
 
-import qrcode "github.com/skip2/go-qrcode"
+import (
+	qrcode "github.com/skip2/go-qrcode"
+)
 
 type QRCodeGenerator interface {
 	GenerateQRCode(content string, level qrcode.RecoveryLevel, size int) ([]byte, error)
