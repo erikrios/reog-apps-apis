1680544dee3ea6f1708c6e142006ae5c7572cf1c
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2022-04-18 20:40:20 +0700

    Implements the Create method

---
+++ service/property/property_service_impl.go
@@ -0,0 +1,80 @@
+package property
+
+import (
+	"context"
+
+	"github.com/erikrios/reog-apps-apis/entity"
+	"github.com/erikrios/reog-apps-apis/model/payload"
+	"github.com/erikrios/reog-apps-apis/repository/group"
+	"github.com/erikrios/reog-apps-apis/repository/property"
+	"github.com/erikrios/reog-apps-apis/service"
+	"github.com/erikrios/reog-apps-apis/utils/generator"
+	"gopkg.in/validator.v2"
+)
+
+type propertyServiceImpl struct {
+	propertyRepository property.PropertyRepository
+	groupRepository    group.GroupRepository
+	idGenerator        generator.IDGenerator
+	qrCodeGenerator    generator.QRCodeGenerator
+}
+
+func NewPropertyServiceImpl(
+	propertyRepository property.PropertyRepository,
+	groupRepository group.GroupRepository,
+	idGenerator generator.IDGenerator,
+	qrCodeGenerator generator.QRCodeGenerator,
+) *propertyServiceImpl {
+	return &propertyServiceImpl{
+		propertyRepository: propertyRepository,
+		groupRepository:    groupRepository,
+		idGenerator:        idGenerator,
+		qrCodeGenerator:    qrCodeGenerator,
+	}
+}
+
+func (p *propertyServiceImpl) Create(ctx context.Context, groupID string, payload payload.CreateProperty) (id string, err error) {
+	if validateErr := validator.Validate(payload); validateErr != nil {
+		err = service.ErrInvalidPayload
+		return
+	}
+
+	if _, repoErr := p.groupRepository.FindByID(ctx, groupID); repoErr != nil {
+		err = service.MapError(repoErr)
+		return
+	}
+
+	id, genErr := p.idGenerator.GeneratePropertyID()
+	if genErr != nil {
+		err = service.MapError(genErr)
+		return
+	}
+
+	property := entity.Property{
+		ID:          id,
+		Name:        payload.Name,
+		Description: payload.Description,
+		Amount:      payload.Amount,
+		GroupID:     groupID,
+	}
+
+	if repoErr := p.propertyRepository.Insert(ctx, property); repoErr != nil {
+		err = service.MapError(repoErr)
+	}
+
+	return
+}
+
+func (p *propertyServiceImpl) Update(ctx context.Context, id string, payload payload.UpdateProperty) (err error) {
+	if validateErr := validator.Validate(payload); validateErr != nil {
+		err = service.ErrInvalidPayload
+		return
+	}
+	return
+}
+
+func (p *propertyServiceImpl) Delete(ctx context.Context, id string) (err error) { return }
+
+func (p *propertyServiceImpl) GeterateQRCode(ctx context.Context, id string) (file []byte, err error) {
+	return
+}
