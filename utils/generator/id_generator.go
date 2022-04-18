4f4884c71f1b10ab559ea3acc71ab53bdea04c49
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2022-04-18 12:16:15 +0700

    Refactor the ID generator

--- utils/generator/id_generator.go
+++ utils/generator/id_generator.go
@@ -6,31 +6,45 @@ import (
 	"github.com/aidarkhanov/nanoid/v2"
 )
 
-func GenerateGroupID() (id string, err error) {
-	id, err = generate(3)
+type IDGenerator interface {
+	GenerateGroupID() (id string, err error)
+	GenerateAdminID(id string, err error)
+	GenerateAddressID() (id string, err error)
+	GeneratePropertyID() (id string, err error)
+}
+
+type nanoidIDGenerator struct {
+}
+
+func NewNanoidIDGenerator() *nanoidIDGenerator {
+	return &nanoidIDGenerator{}
+}
+
+func (n *nanoidIDGenerator) GenerateGroupID() (id string, err error) {
+	id, err = n.generate(3)
 	id = fmt.Sprintf("g-%s", id)
 	return
 }
 
-func GenerateAdminID(id string, err error) {
-	id, err = generate(2)
+func (n *nanoidIDGenerator) GenerateAdminID(id string, err error) {
+	id, err = n.generate(2)
 	id = fmt.Sprintf("u-%s", id)
 	return
 }
 
-func GenerateAddressID() (id string, err error) {
-	id, err = generate(3)
+func (n *nanoidIDGenerator) GenerateAddressID() (id string, err error) {
+	id, err = n.generate(3)
 	id = fmt.Sprintf("a-%s", id)
 	return
 }
 
-func GeneratePropertyID() (id string, err error) {
-	id, err = generate(7)
+func (n *nanoidIDGenerator) GeneratePropertyID() (id string, err error) {
+	id, err = n.generate(7)
 	id = fmt.Sprintf("p-%s", id)
 	return
 }
 
-func generate(size int) (id string, err error) {
+func (n *nanoidIDGenerator) generate(size int) (id string, err error) {
 	id, err = nanoid.GenerateString(nanoid.DefaultAlphabet, size)
 	return
 }
