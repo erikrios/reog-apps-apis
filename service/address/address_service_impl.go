f274b34db2d9f47930fea1d451b8051ceadebf0a
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2022-04-18 19:23:42 +0700

    Create the implementation of address_service.go interface

---
+++ service/address/address_service_impl.go
@@ -0,0 +1,53 @@
+package address
+
+import (
+	"context"
+
+	"github.com/erikrios/reog-apps-apis/entity"
+	"github.com/erikrios/reog-apps-apis/model/payload"
+	"github.com/erikrios/reog-apps-apis/repository/address"
+	"github.com/erikrios/reog-apps-apis/repository/village"
+	"github.com/erikrios/reog-apps-apis/service"
+)
+
+type addressServiceImpl struct {
+	addressRepository address.AddressRepository
+	villageRepository village.VillageRepository
+}
+
+func NewAddressServiceImpl(
+	addressRepository address.AddressRepository,
+	villageRepository village.VillageRepository,
+) *addressServiceImpl {
+	return &addressServiceImpl{
+		addressRepository: addressRepository,
+		villageRepository: villageRepository,
+	}
+}
+
+func (a *addressServiceImpl) Update(ctx context.Context, id string, p payload.UpdateAddress) (err error) {
+	village, villageErr := a.villageRepository.FindByID(p.VillageID)
+	if villageErr != nil {
+		err = service.MapError(villageErr)
+		return
+	}
+
+	address := entity.Address{
+		ID:           id,
+		Address:      p.Address,
+		VillageID:    village.ID,
+		VillageName:  village.Name,
+		DistrictID:   village.District.ID,
+		DistrictName: village.District.Name,
+		RegencyID:    village.District.Regency.ID,
+		RegencyName:  village.District.Regency.Name,
+		ProvinceID:   village.District.Regency.Province.ID,
+		ProvinceName: village.District.Regency.Province.Name,
+	}
+
+	repoErr := a.addressRepository.Update(ctx, id, address)
+	if repoErr != nil {
+		err = service.MapError(repoErr)
+	}
+	return
+}
