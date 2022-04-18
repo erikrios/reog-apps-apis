c5beb5b691dbf5fe766e7f9a3380d38c457bd03d
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2022-04-18 20:34:49 +0700

    Change the validation rule

--- model/payload/property.go
+++ model/payload/property.go
@@ -3,11 +3,11 @@ package payload
 type CreateProperty struct {
 	Name        string `json:"name" validate:"nonzero,min=2,max=80"`
 	Description string `json:"description" validate:"nonzero,min=2,max=1000"`
-	Amount      uint16 `json:"amount" validate:"nonzero,min=2"`
+	Amount      uint16 `json:"amount" validate:"nonzero,min=1"`
 }
 
 type UpdateProperty struct {
 	Name        string `json:"name" validate:"nonzero,min=2,max=80"`
 	Description string `json:"description" validate:"nonzero,min=2,max=1000"`
-	Amount      uint16 `json:"amount" validate:"nonzero,min=2"`
+	Amount      uint16 `json:"amount" validate:"nonzero,min=1"`
 }
