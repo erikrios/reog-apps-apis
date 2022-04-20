fc98129bb9dec0a7a88ea91efcb778f1a9c66cb5
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2022-04-20 08:05:13 +0700

    Create the instance of echo and initialize the Swagger API Documentation

--- main.go
+++ main.go
@@ -2,12 +2,31 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/erikrios/reog-apps-apis/config"
+	_ "github.com/erikrios/reog-apps-apis/docs"
+	"github.com/erikrios/reog-apps-apis/middleware"
 	_ "github.com/erikrios/reog-apps-apis/validation"
 	"github.com/joho/godotenv"
+	"github.com/labstack/echo/v4"
+	echoSwagger "github.com/swaggo/echo-swagger"
 )
 
+// @title           Reog Apps API
+// @version         1.0
+// @description     API for Reog Group in Ponorogo
+// @termsOfService  http://swagger.io/terms/
+
+// @contact.name   Erik Rio Setiawan
+// @contact.url    http://www.swagger.io/support
+// @contact.email  erikriosetiawan15@gmail.com
+
+// @license.name  Apache 2.0
+// @license.url   http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host      localhost
+// @BasePath  /api/v1
 func main() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 	if err := godotenv.Load(".env"); err != nil {
@@ -23,4 +42,26 @@ func main() {
 
 	config.MigratePostgreSQLDatabase(db)
 	config.SetInitialDataPostgreSQLDatabase(db)
+
+	port := ":" + os.Getenv("PORT")
+
+	e := echo.New()
+
+	if os.Getenv("ENV") == "production" {
+		middleware.BodyLimit(e)
+		middleware.Gzip(e)
+		middleware.RateLimiter(e)
+		middleware.Recover(e)
+		middleware.Secure(e)
+		middleware.RemoveTrailingSlash(e)
+	} else {
+		middleware.Logger(e)
+		middleware.RemoveTrailingSlash(e)
+	}
+
+	e.GET("/*", echoSwagger.WrapHandler)
+
+	_ = e.Group("/api/v1")
+
+	e.Logger.Fatal(e.Start(port))
 }
