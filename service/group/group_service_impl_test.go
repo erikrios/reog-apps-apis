5bc9746a7e38883b8530a8fcaf1a7363523b2e88
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2022-04-23 18:50:04 +0700

    Create the unit testing for FindByID method

--- service/group/group_service_impl_test.go
+++ service/group/group_service_impl_test.go
@@ -315,3 +315,148 @@ func TestGetAll(t *testing.T) {
 		})
 	}
 }
+
+func TestGetByID(t *testing.T) {
+	mockGroupRepo := &mgr.GroupRepository{}
+	mockVillageRepo := &mvr.VillageRepository{}
+	mockIDGen := &mig.IDGenerator{}
+	mockQRGen := &mqg.QRCodeGenerator{}
+
+	var groupService GroupService = NewGroupServiceImpl(
+		mockGroupRepo,
+		mockVillageRepo,
+		mockIDGen,
+		mockQRGen,
+	)
+
+	testCases := []struct {
+		name           string
+		inputID        string
+		expectedGroup  response.Group
+		expectedError  error
+		mockBehaviours func()
+	}{
+		{
+			name:          "it should return service.ErrRepository error, when group repository return an error",
+			inputID:       "g-xyz",
+			expectedGroup: response.Group{},
+			expectedError: service.ErrRepository,
+			mockBehaviours: func() {
+				mockGroupRepo.On(
+					"FindByID",
+					mock.AnythingOfType(fmt.Sprintf("%T", context.Background())),
+					mock.AnythingOfType(fmt.Sprintf("%T", "")),
+				).Return(
+					func(ctx context.Context, id string) entity.Group {
+						return entity.Group{}
+					},
+					func(ctx context.Context, id string) error {
+						return repository.ErrDatabase
+					},
+				).Once()
+			},
+		},
+		{
+			name:          "it should return service.ErrDataNotFound error, when group repository return an error",
+			inputID:       "g-xyz",
+			expectedGroup: response.Group{},
+			expectedError: service.ErrDataNotFound,
+			mockBehaviours: func() {
+				mockGroupRepo.On(
+					"FindByID",
+					mock.AnythingOfType(fmt.Sprintf("%T", context.Background())),
+					mock.AnythingOfType(fmt.Sprintf("%T", "")),
+				).Return(
+					func(ctx context.Context, id string) entity.Group {
+						return entity.Group{}
+					},
+					func(ctx context.Context, id string) error {
+						return repository.ErrRecordNotFound
+					},
+				).Once()
+			},
+		},
+		{
+			name:    "it should return a valid group, when no error is returned",
+			inputID: "g-xyz",
+			expectedGroup: response.Group{
+				ID:     "g-Nzo",
+				Name:   "Paguyuban Reog",
+				Leader: "Erik Rio Setiawan",
+				Address: response.Address{
+					ID:           "g-Nzo",
+					Address:      "RT 05 RW 01 Dukuh Tengah",
+					VillageID:    "3502030007",
+					VillageName:  "Pager",
+					DistrictID:   "3502030",
+					DistrictName: "Bungkal",
+					RegencyID:    "3502",
+					RegencyName:  "Kabupaten Ponorogo",
+					ProvinceID:   "35",
+					ProvinceName: "Jawa Timur",
+				},
+				Properties: []response.Property{
+					{
+						ID:          "p-YIhpPgp",
+						Name:        "Dadak Merak",
+						Description: "Ini adalah deskripsi dari dadak merak",
+						Amount:      1,
+					},
+				},
+			},
+			expectedError: nil,
+			mockBehaviours: func() {
+				mockGroupRepo.On(
+					"FindByID",
+					mock.AnythingOfType(fmt.Sprintf("%T", context.Background())),
+					mock.AnythingOfType(fmt.Sprintf("%T", "")),
+				).Return(
+					func(ctx context.Context, id string) entity.Group {
+						return entity.Group{
+							ID:     "g-Nzo",
+							Name:   "Paguyuban Reog",
+							Leader: "Erik Rio Setiawan",
+							Address: entity.Address{
+								ID:           "g-Nzo",
+								Address:      "RT 05 RW 01 Dukuh Tengah",
+								VillageID:    "3502030007",
+								VillageName:  "Pager",
+								DistrictID:   "3502030",
+								DistrictName: "Bungkal",
+								RegencyID:    "3502",
+								RegencyName:  "Kabupaten Ponorogo",
+								ProvinceID:   "35",
+								ProvinceName: "Jawa Timur",
+							},
+							Properties: []entity.Property{
+								{
+									ID:          "p-YIhpPgp",
+									Name:        "Dadak Merak",
+									Description: "Ini adalah deskripsi dari dadak merak",
+									Amount:      1,
+								},
+							},
+						}
+					},
+					func(ctx context.Context, id string) error {
+						return nil
+					},
+				).Once()
+			},
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			testCase.mockBehaviours()
+			gotGroup, gotErr := groupService.GetByID(context.Background(), testCase.inputID)
+
+			if testCase.expectedError != nil {
+				assert.ErrorIs(t, gotErr, testCase.expectedError)
+			} else {
+				assert.NoError(t, gotErr)
+				assert.Equal(t, testCase.expectedGroup, gotGroup)
+			}
+		})
+	}
+}
