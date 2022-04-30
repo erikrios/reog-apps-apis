24dae70aade6c0776d072c1c3a1e5e69e3a0b9b2
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2022-04-30 20:46:19 +0700

    Refactor the code

--- model/response/show_schedule.go
+++ model/response/show_schedule.go
@@ -1,23 +1,22 @@
 package response
 
-import "time"
-
 type ShowSchedule struct {
 	ID      string `json:"id" extensions:"x-order=0"`
 	GroupID string `json:"groupID" extensions:"x-order=1"`
 	Place   string `json:"place" extensions:"x-order=2"`
 	// StartOn layout format: time.RFC822 (02 Jan 06 15:04 MST)
-	StartOn time.Time `json:"startOn" extensions:"x-order=3"`
+	StartOn string `json:"startOn" extensions:"x-order=3"`
 	// FinishOn layout format: time.RFC822 (02 Jan 06 15:04 MST)
-	FinishOn time.Time `json:"finishOn" extensions:"x-order=4"`
+	FinishOn string `json:"finishOn" extensions:"x-order=4"`
 }
 
 type ShowScheduleDetails struct {
-	ID    string `json:"id" extensions:"x-order=0"`
-	Group Group  `json:"group" extensions:"x-order=1"`
-	Place string `json:"place" extensions:"x-order=2"`
+	ID        string `json:"id" extensions:"x-order=0"`
+	GroupID   string `json:"groupID" extensions:"x-order=1"`
+	GroupName string `json:"groupName" extensions:"x-order=2"`
+	Place     string `json:"place" extensions:"x-order=3"`
 	// StartOn layout format: time.RFC822 (02 Jan 06 15:04 MST)
-	StartOn time.Time `json:"startOn" extensions:"x-order=3"`
+	StartOn string `json:"startOn" extensions:"x-order=4"`
 	// FinishOn layout format: time.RFC822 (02 Jan 06 15:04 MST)
-	FinishOn time.Time `json:"finishOn" extensions:"x-order=4"`
+	FinishOn string `json:"finishOn" extensions:"x-order=5"`
 }
