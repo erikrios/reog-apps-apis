2e5970c097a0edd411f262ee19b467c88ee40665
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2022-04-30 11:12:35 +0700

    Create show_schedule.go

--- model/response/show_schedule.go
+++ model/response/show_schedule.go
@@ -4,12 +4,12 @@ import "time"
 
 type ShowSchedule struct {
 	ID      string `json:"id" extensions:"x-order=0"`
-	GroupID string `json:"groupID" extensions:"x-order=0"`
-	Place   string `json:"place" extensions:"x-order=0"`
+	GroupID string `json:"groupID" extensions:"x-order=1"`
+	Place   string `json:"place" extensions:"x-order=2"`
 	// StartOn layout format: time.RFC822 (02 Jan 06 15:04 MST)
-	StartOn time.Time `json:"startOn" extensions:"x-order=0"`
+	StartOn time.Time `json:"startOn" extensions:"x-order=3"`
 	// FinishOn layout format: time.RFC822 (02 Jan 06 15:04 MST)
-	FinishOn time.Time `json:"finishOn" extensions:"x-order=0"`
+	FinishOn time.Time `json:"finishOn" extensions:"x-order=4"`
 }
 
 type ShowScheduleDetails struct {
