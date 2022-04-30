e142cc9d548a43ee47651dbfe9f0005ca4c3f986
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2022-04-30 11:24:31 +0700

    Create show_schedule_service.go

--- service/showschedule/show_schedule_service.go
+++ service/showschedule/show_schedule_service.go
@@ -9,14 +9,9 @@ import (
 
 type ShowScheduleService interface {
 	Create(ctx context.Context, p payload.CreateShowSchedule) (id string, err error)
-
 	GetAll(ctx context.Context) (responses []response.ShowSchedule, err error)
-
 	GetByID(ctx context.Context, id string) (response response.ShowScheduleDetails, err error)
-
 	GetByGroupID(ctx context.Context, groupID string) (responses []response.ShowSchedule, err error)
-
 	Update(ctx context.Context, id string, p payload.UpdateShowSchedule) (err error)
-
 	Delete(ctx context.Context, id string) (err error)
 }
