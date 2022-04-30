c27c660b0f2dd758053bd530b71a030f5a5069ac
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2022-04-30 11:09:17 +0700

    Create show_schedule.go

--- model/response/show_schedule.go
+++ model/response/show_schedule.go
@@ -3,9 +3,17 @@ package response
 import "time"
 
 type ShowSchedule struct {
-	ID       string `json:"id" extensions:"x-order=0"`
-	GroupID  string
-	Place    string
-	StartOn  time.Time
-	FinishOn time.Time
+	ID       string    `json:"id" extensions:"x-order=0"`
+	GroupID  string    `json:"groupID" extensions:"x-order=0"`
+	Place    string    `json:"place" extensions:"x-order=0"`
+	StartOn  time.Time `json:"startOn" extensions:"x-order=0"`
+	FinishOn time.Time `json:"finishOn" extensions:"x-order=0"`
+}
+
+type ShowScheduleDetails struct {
+	ID       string    `json:"id" extensions:"x-order=0"`
+	Group    Group     `json:"group" extensions:"x-order=1"`
+	Place    string    `json:"place" extensions:"x-order=2"`
+	StartOn  time.Time `json:"startOn" extensions:"x-order=3"`
+	FinishOn time.Time `json:"finishOn" extensions:"x-order=4"`
 }
