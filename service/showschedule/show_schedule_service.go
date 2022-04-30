0b548d22dcb41166235c97080e11f06616aaba04
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2022-04-30 11:23:27 +0700

    Create property_service_impl.go

--- service/showschedule/show_schedule_service.go
+++ service/showschedule/show_schedule_service.go
@@ -8,10 +8,10 @@ import (
 )
 
 type ShowScheduleService interface {
-	Create(ctx context.Context, s payload.CreateShowSchedule) (id string, err error)
+	Create(ctx context.Context, p payload.CreateShowSchedule) (id string, err error)
 	GetAll(ctx context.Context) (responses []response.ShowSchedule, err error)
 	GetByID(ctx context.Context, id string) (response response.ShowScheduleDetails, err error)
 	GetByGroupID(ctx context.Context, groupID string) (responses []response.ShowSchedule, err error)
-	Update(ctx context.Context, id string, s payload.UpdateShowSchedule) (err error)
+	Update(ctx context.Context, id string, p payload.UpdateShowSchedule) (err error)
 	Delete(ctx context.Context, id string) (err error)
 }
