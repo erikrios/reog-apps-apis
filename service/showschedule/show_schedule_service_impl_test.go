aad8cfa8e9b2850a0ecc899c87211f41385d75a5
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2022-05-06 18:39:14 +0700

    Add the unit testing for GetByID method

--- service/showschedule/show_schedule_service_impl_test.go
+++ service/showschedule/show_schedule_service_impl_test.go
@@ -325,6 +325,159 @@ func TestGetAll(t *testing.T) {
 }
 
 func TestGetByID(t *testing.T) {
+	mockShowScheduleRepo := &mssr.ShowScheduleRepository{}
+	mockGroupRepo := &mgr.GroupRepository{}
+	mockIDGen := &mig.IDGenerator{}
+
+	var showScheduleService ShowScheduleService = NewShowScheduleServiceImpl(
+		mockShowScheduleRepo,
+		mockGroupRepo,
+		mockIDGen,
+	)
+
+	testCases := []struct {
+		name                 string
+		expectedShowSchedule response.ShowScheduleDetails
+		expectedError        error
+		mockBehaviours       func()
+	}{
+		{
+			name:          "it should return service.ErrDataNotFound error, when show schedule repository return an error",
+			expectedError: service.ErrDataNotFound,
+			mockBehaviours: func() {
+				mockShowScheduleRepo.On(
+					"FindByID",
+					mock.AnythingOfType(fmt.Sprintf("%T", context.Background())),
+					mock.AnythingOfType(fmt.Sprintf("%T", "")),
+				).Return(
+					func(ctx context.Context, id string) entity.ShowSchedule {
+						return entity.ShowSchedule{}
+					},
+					func(ctx context.Context, id string) error {
+						return repository.ErrRecordNotFound
+					},
+				).Once()
+			},
+		},
+		{
+			name:          "it should return service.ErrRepository error, when show schedule repository return an error",
+			expectedError: service.ErrRepository,
+			mockBehaviours: func() {
+				mockShowScheduleRepo.On(
+					"FindByID",
+					mock.AnythingOfType(fmt.Sprintf("%T", context.Background())),
+					mock.AnythingOfType(fmt.Sprintf("%T", "")),
+				).Return(
+					func(ctx context.Context, id string) entity.ShowSchedule {
+						return entity.ShowSchedule{}
+					},
+					func(ctx context.Context, id string) error {
+						return repository.ErrDatabase
+					},
+				).Once()
+			},
+		},
+		{
+			name:          "it should return service.ErrRepository error, when group repository return an error",
+			expectedError: service.ErrRepository,
+			mockBehaviours: func() {
+				mockShowScheduleRepo.On(
+					"FindByID",
+					mock.AnythingOfType(fmt.Sprintf("%T", context.Background())),
+					mock.AnythingOfType(fmt.Sprintf("%T", "")),
+				).Return(
+					func(ctx context.Context, id string) entity.ShowSchedule {
+						return entity.ShowSchedule{
+							ID:       "s-EuKgD1O",
+							GroupID:  "g-xyz",
+							Place:    "Lapangan Bungkal",
+							StartOn:  time.Now(),
+							FinishOn: time.Now().Add(3 * time.Hour),
+						}
+					},
+					func(ctx context.Context, id string) error {
+						return nil
+					},
+				).Once()
+
+				mockGroupRepo.On(
+					"FindByID",
+					mock.AnythingOfType(fmt.Sprintf("%T", context.Background())),
+					mock.AnythingOfType(fmt.Sprintf("%T", "")),
+				).Return(
+					func(ctx context.Context, id string) entity.Group {
+						return entity.Group{}
+					},
+					func(ctx context.Context, id string) error {
+						return repository.ErrDatabase
+					},
+				).Once()
+			},
+		},
+		{
+			name:          "it should return a valid show schedules, when no error is returned",
+			expectedError: nil,
+			expectedShowSchedule: response.ShowScheduleDetails{
+				ID:        "s-EuKgD1O",
+				GroupID:   "g-xyz",
+				GroupName: "Paguyuban Reog",
+				Place:     "Lapangan Bungkal",
+				StartOn:   time.Now().Format(time.RFC822),
+				FinishOn:  time.Now().Add(3 * time.Hour).Format(time.RFC822),
+			},
+			mockBehaviours: func() {
+				mockShowScheduleRepo.On(
+					"FindByID",
+					mock.AnythingOfType(fmt.Sprintf("%T", context.Background())),
+					mock.AnythingOfType(fmt.Sprintf("%T", "")),
+				).Return(
+					func(ctx context.Context, id string) entity.ShowSchedule {
+						return entity.ShowSchedule{
+							ID:       "s-EuKgD1O",
+							GroupID:  "g-xyz",
+							Place:    "Lapangan Bungkal",
+							StartOn:  time.Now(),
+							FinishOn: time.Now().Add(3 * time.Hour),
+						}
+					},
+					func(ctx context.Context, id string) error {
+						return nil
+					},
+				).Once()
+
+				mockGroupRepo.On(
+					"FindByID",
+					mock.AnythingOfType(fmt.Sprintf("%T", context.Background())),
+					mock.AnythingOfType(fmt.Sprintf("%T", "")),
+				).Return(
+					func(ctx context.Context, id string) entity.Group {
+						return entity.Group{
+							ID:   "g-xyz",
+							Name: "Paguyuban Reog",
+						}
+					},
+					func(ctx context.Context, id string) error {
+						return nil
+					},
+				).Once()
+			},
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			testCase.mockBehaviours()
+
+			gotShowSchedule, gotErr := showScheduleService.GetByID(context.Background(), "g-xyz")
+
+			if testCase.expectedError != nil {
+				assert.ErrorIs(t, gotErr, testCase.expectedError)
+			} else {
+				assert.NoError(t, gotErr)
+				assert.Equal(t, testCase.expectedShowSchedule, gotShowSchedule)
+			}
+		})
+	}
 }
 
 func TestGetByGroupID(t *testing.T) {
